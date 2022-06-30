de311f85679cb9552a2a090db26d91cabb451f49
Douglas Porfiro <douglasporfiro25@gmail.com>
2022-06-30 09:07:24 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,12 +13,12 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
-INSERT INTO tb_notification (text, moment, read, route, tb_user) VALUES ('Nova mensagem', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', false, 0, 1);
+INSERT INTO tb_notification (text, moment, read, route, tb_user) VALUES ('Nova mensagem', '2020-11-20T03:00:00Z', true, 0, 1);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('curso online', 'https://c.pxhere.com/images/cb/4f/09ab0eff60dade28fd17768aeb16-1570023.jpg!d', 'https://blogcorretor.aocubo.com/wp-content/uploads/2022/01/aaasd-1140x735.jpg' );
 
-INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
-INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', '2020-11-20T03:00:00Z', '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', '2020-12-20T03:00:00Z', '2021-12-20T03:00:00Z', 1);
 
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://c.pxhere.com/images/cb/4f/09ab0eff60dade28fd17768aeb16-1570023.jpg!d', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire as suas dúvidas', 2, 'https://c.pxhere.com/images/cb/4f/09ab0eff60dade28fd17768aeb16-1570023.jpg!d', 2, 1);
@@ -27,3 +27,7 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, tb_resource, tb_prerequisite) VALUES ('Capítulo 1', 'Neste capítulo vamos começar', 1, 'https://c.pxhere.com/images/cb/4f/09ab0eff60dade28fd17768aeb16-1570023.jpg!d', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, tb_resource, tb_prerequisite) VALUES ('Capítulo 2', 'Neste capítulo vamos continuar', 2, 'https://c.pxhere.com/images/cb/4f/09ab0eff60dade28fd17768aeb16-1570023.jpg!d', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, tb_resource, tb_prerequisite) VALUES ('Capítulo 3', 'Neste capítulo vamos finalizar', 3, 'https://c.pxhere.com/images/cb/4f/09ab0eff60dade28fd17768aeb16-1570023.jpg!d', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, '2020-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, '2020-11-20T13:00:00Z', null, true, false);
+
